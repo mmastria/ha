25c0bda0848f3f6e6fce7e980de5baf0bc449c3d
root <marco@mastria.com.br>
2018-08-19 09:36:26 +0000

    system-power bugs

--- setup1.sh
+++ setup1.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # etcher raspibian stretch
 # touch /boot/ssh
@@ -40,22 +40,22 @@ passwd <<eof
 0
 eof
 
-sed -i '/^PermitRootLogin/ c\PermitRootLogin Yes' /etc/ssh/sshd_config
-systemctl enable ssh
+sed -i '/^#PermitRootLogin/ c\PermitRootLogin Yes' /etc/ssh/sshd_config
+#systemctl enable ssh
 systemctl restart ssh
 
 apt -y upgrade
 apt -y dist-upgrade
 rpi-update
 
-sed -i '/raspberrypi/ c\127.0.0.1 %SDEVICE %SDEVICE.local' /etc/hosts
-echo %1 > /etc/hostname
+sed -i $'/raspberrypi/ c127.0.0.1\t$SDEVICE $SDEVICE.local' /etc/hosts
+echo $SDEVICENAME > /etc/hostname
 sed -i 's/^# export/export/g' /root/.bashrc
 sed -i 's/^# eval/eval/g' /root/.bashrc
 sed -i 's/^# alias l/alias l/g' /root/.bashrc
 
 apt -y install build-essential git python-dev python-pip vim \
-       install cdbs libcfitsio-dev libnova-dev \
+       cdbs libcfitsio-dev libnova-dev \
        libusb-1.0-0-dev libjpeg-dev libusb-dev libtiff5-dev \
        libftdi1-dev fxload libkrb5-dev libcurl4-gnutls-dev \
        libraw-dev libgphoto2-dev libgsl-dev dkms \
@@ -67,13 +67,14 @@ ln -s /usr/bin/swig2.0 /usr/bin/swig
 # apt -y install indi-full
 # pip2 install --upgrade pip
 
-pip install --install-option="--prefix=/usr/local" pyindi-client 
+cd /root
+curl http://indilib.org/download/raspberry-pi/send/6-raspberry-pi/9-indi-library-for-raspberry-pi.html -o libindi_rpi.tar.gz
+tar -zxvf libindi_rpi.tar.gz
+cd libindi_1*
+dpkg -i *.deb
 
 cd /root
-curl http://indilib.org/download/raspberry-pi/send/6-raspberry-pi/9-indi-library-for-raspberry-pi.html -o libindi_1.7.3_rpi.tar.gz
-tar -zxvf libindi_1.7.3_rpi.tar.gz
-cd libindi_1.7.3_rpi
-dpkg -i *.deb
+pip install --install-option="--prefix=/usr/local" pyindi-client 
 
 read -p "key to reboot"
 reboot
