e679f8c59b6234b5ff0268a270272cd8091293ef
root <marco@mastria.com.br>
2018-08-19 16:43:34 +0000

    7625

--- system-west/arua_system-west_indiserver.sh
+++ system-west/arua_system-west_indiserver.sh
@@ -15,13 +15,13 @@ mkfifo ~/.indi/indiFIFO
 /usr/bin/indiserver -p 7624 -m 100 -v -f ~/.indi/indiFIFO &
 child=$!
 
-echo "start indi_ioptronv3_telescope -n \"iOptron CEM120\" -c \"~/.indi/iOptron CEM120_config.xml\"" > ~/.indi/indiFIFO
-echo "start indi_atik_ccd -n \"Atik 320E\" -c \"~/.indi/Atik 320E_config.xml\"" > ~/.indi/indiFIFO
-echo "start indi_qhy_ccd -n \"QHY5LII-M\" -c \"~/.indi/QHY5LII-M_config.xml\"" > ~/.indi/indiFIFO
-echo "start \"Armadillo focuser\"@system-main:7626  -n \"Armadillo focuser\" -c \"~/.indi/Armadillo focuser_config.xml\"" > ~/.indi/indiFIFO
-echo "start \"Astrometry\"@system-main -n \"Astrometry\" -c \"~/.indi/Astrometry_config.xml\"" > ~/.indi/indiFIFO
-echo "start \"AAG Cloud Watcher\"@aagsolo -n \"AAG Cloud Watcher\" -c \"~/.indi/AAG Cloud Watcher_config.xml\"" > ~/.indi/indiFIFO
-echo "start \"RollOff Roof\"@system-power -n \"RollOff Roof\" -c \"~/.indi/RollOff Roof_config.xml\"" > ~/.indi/indiFIFO
+echo "start indi_ioptronv3_telescope -n \"iOptron CEM120\" -c \"$HOME/.indi/iOptron CEM120_config.xml\"" > ~/.indi/indiFIFO
+echo "start indi_atik_ccd -n \"Atik 320E\" -c \"$HOME/.indi/Atik 320E_config.xml\"" > ~/.indi/indiFIFO
+echo "start indi_qhy_ccd -n \"QHY5LII-M\" -c \"$HOME/.indi/QHY5LII-M_config.xml\"" > ~/.indi/indiFIFO
+echo "start \"Armadillo focuser\"@system-main:7626" > ~/.indi/indiFIFO
+echo "start \"Astrometry\"@system-main" > ~/.indi/indiFIFO
+echo "start \"AAG Cloud Watcher\"@aagsolo" > ~/.indi/indiFIFO
+echo "start \"RollOff Roof\"@system-power:7625" > ~/.indi/indiFIFO
 
 wait "$child"
 echo "stopping indiserver"
