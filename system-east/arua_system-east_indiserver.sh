ae9bba36ac7705b73358babbb9beaf4ae2c4d8c3
root <marco@mastria.com.br>
2018-08-28 19:37:28 -0300

    revert ioptron driver ieq

--- system-east/arua_system-east_indiserver.sh
+++ system-east/arua_system-east_indiserver.sh
@@ -15,8 +15,8 @@ mkfifo ~/.indi/indiFIFO
 /usr/bin/indiserver -p 7624 -m 100 -v -f ~/.indi/indiFIFO &
 child=$!
 
-#echo "start indi_ieq_telescope -n \"iOptron CEM60\" -c \"$HOME/.indi/iOptron CEM60_config.xml\"" > ~/.indi/indiFIFO
-echo "start indi_ioptronv3_telescope -n \"iOptron CEM60\" -c \"$HOME/.indi/iOptron CEM60_config.xml\"" > ~/.indi/indiFIFO
+echo "start indi_ieq_telescope -n \"iOptron CEM60\" -c \"$HOME/.indi/iOptron CEM60_config.xml\"" > ~/.indi/indiFIFO
+#echo "start indi_ioptronv3_telescope -n \"iOptron CEM60\" -c \"$HOME/.indi/iOptron CEM60_config.xml\"" > ~/.indi/indiFIFO
 echo "start indi_atik_ccd -n \"Atik 383L+\" -c \"$HOME/.indi/Atik 383L+_config.xml\"" > ~/.indi/indiFIFO
 echo "start indi_sx_wheel -n \"SX Wheel\" -c \"$HOME/.indi/SX Wheel_config.xml\"" > ~/.indi/indiFIFO
 echo "start indi_qhy_ccd -n \"QHY5LII-M\" -c \"$HOME/.indi/QHY5LII-M_config.xml\"" > ~/.indi/indiFIFO
