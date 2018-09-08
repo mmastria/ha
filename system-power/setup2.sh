b3821ef38b6c505215d92668027bad25740e8ce2
root <marco@mastria.com.br>
2018-09-07 22:56:12 -0300

    refactor indi_duino

--- system-power/setup2.sh
+++ system-power/setup2.sh
@@ -27,6 +27,35 @@ cp -f *.xml /usr/share/indi/
 cp -f *.py /usr/local/bin/
 cp -f arua*.sh /usr/local/bin/
 
+grep AAG /usr/share/indi/indi_duino.xml > /dev/null 2>&1 || \
+cp -f /usr/share/indi/indi_duino.xml /usr/share/indi/indi_duino.xml.ORIGINAL
+
+grep AAG /usr/share/indi/indi_duino.xml > /dev/null 2>&1 || \
+sed -i '/<device label="Arduino Servo" skel="servo_sk.xml">/i \
+        <device label="Power HUB" skel="arua_powerhub_sk.xml"> \
+               <driver name="Power HUB">indi_duino</driver> \
+               <version>0.1</version> \
+        </device>
+' /usr/share/indi/indi_duino.xml
+
+grep AAG /usr/share/indi/indi_duino.xml > /dev/null 2>&1 || \
+sed -i '/<devGroup group="Weather">/i \
+<devGroup group="Domes"> \
+       <device label="RollOff Roof" skel="arua_rolloffroof_sk.xml"> \
+               <driver name="RollOff Roof">indi_duino</driver> \
+               <version>0.1</version> \
+       </device> \
+</devGroup> 
+' /usr/share/indi/indi_duino.xml
+
+grep AAG /usr/share/indi/indi_duino.xml > /dev/null 2>&1 || \
+sed -i '/<device label="Arduino MeteoStation" skel="meteostation_sk.xml">/i \
+    <device label="AAG Weather Safe" skel="arua_aagsafe_sk.xml"> \
+            <driver name="AAG Weather Safe">indi_duino</driver> \
+            <version>0.1</version> \
+    </device>
+' /usr/share/indi/indi_duino.xml
+
 systemctl daemon-reload
 
 systemctl enable arua_powerhub_indiserver.service
@@ -36,3 +65,5 @@ systemctl enable arua_rolloffroof_monitor.service
 systemctl restart arua_powerhub_indiserver.service
 systemctl restart arua_rolloffroof_indiserver.service
 systemctl restart arua_rolloffroof_monitor.service
+
+
