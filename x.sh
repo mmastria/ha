15bf06d4a653f3f6330753a5d64be2b2848096b9
root <marco@mastria.com.br>
2018-09-16 02:43:52 -0300

    remove x

--- x.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $# -eq 0 -o ! -d "$1" ];then echo -e "$0 <system-xxxx>";exit 1;fi
-SDEVICE="${1///}"
-SSED=$(echo "/raspberrypi/ c127.0.0.1\t$SDEVICE \t$SDEVICE.local")
-
-sed -i $"$SSED" /etc/hosts
-echo $SDEVICE > /etc/hostname
-
