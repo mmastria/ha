f819d43c90dc075506e467fb36ec0d2b76be2b29
root <marco@mastria.com.br>
2018-09-16 00:00:40 -0300

    align scripts

--- system-west/setup2.sh
+++ system-west/setup2.sh
@@ -11,7 +11,7 @@
 [ $(grep west /etc/passwd|wc -l) -eq 0 ] && \
 useradd -m -s /bin/bash west && \
 adduser west dialout && \
-adduser west gpio
+adduser west gpio && \
 sed -i 's/^#alias l/alias l/g' /home/west/.bashrc
 
 cp -f *.service /etc/systemd/system/
@@ -20,11 +20,4 @@ cp -f arua*.sh /usr/local/bin/
 systemctl daemon-reload
 
 systemctl enable arua_system-west_indiserver.service
-#systemctl enable arua_cem120.service
-#systemctl enable arua_qhy5lii.service
-#systemctl enable arua_atik320e.service
-
 systemctl restart arua_system-west_indiserver.service
-#systemctl restart arua_cem120.service
-#systemctl restart arua_qhy5lii.service
-#systemctl restart arua_atik320e.service
