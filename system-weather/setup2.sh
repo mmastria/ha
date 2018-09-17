03ee4ffed1dc8f280739bc5e510890b3fc00ff5d
mmastria <marco@mastria.com.br>
2018-09-16 23:56:20 -0300

    AAG to USB1

--- system-weather/setup2.sh
+++ system-weather/setup2.sh
@@ -10,6 +10,7 @@ sed -i 's/^#alias l/alias l/g' /home/weather/.bashrc
 
 cp -f *.service /etc/systemd/system/
 cp -f arua*.sh /usr/local/bin/
+sed 's/ttyUSB0/ttyUSB1/' /usr/share/indi/indi_aagcloudwatcher_sk.xml
 
 systemctl daemon-reload
 systemctl enable arua_system-weather_indiserver.service
