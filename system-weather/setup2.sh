685313260fd8b117a3c6402c684d834bd4c0fe6e
mmastria <marco@mastria.com.br>
2018-09-18 03:42:29 -0300

    config udev

--- system-weather/setup2.sh
+++ system-weather/setup2.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# Bus 001 Device 005: ID 067b:2303 Prolific Technology, Inc. PL2303 Serial Port
+[ ! -f /lib/udev/rules.d/99-aagcloudwatcher.rules ] && \
+cat > /lib/udev/rules.d/99-aagcloudwatcher.rules <<- EOF
+# AAG Cloud Watcher udev rule
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="067b", ATTRS{idProduct}=="2303", GROUP="dialout", MODE="0666", SYMLINK+="aagcloudwatcher"
+EOF
+
 [ $(grep weather /etc/passwd|wc -l) -eq 0 ] && \
+
 useradd -m -s /bin/bash weather && \
 adduser weather dialout && \
 adduser weather plugdev && \
@@ -10,8 +18,9 @@ sed -i 's/^#alias l/alias l/g' /home/weather/.bashrc
 
 cp -f *.service /etc/systemd/system/
 cp -f arua*.sh /usr/local/bin/
-sed 's/ttyUSB0/ttyUSB1/' /usr/share/indi/indi_aagcloudwatcher_sk.xml
+sed -i 's/ttyUSB0/aagcloudwatcher/' /usr/share/indi/indi_aagcloudwatcher_sk.xml
 
 systemctl daemon-reload
 systemctl enable arua_system-weather_indiserver.service
 systemctl restart arua_system-weather_indiserver.service
+
