28251f9f27145a2b13fa99011e6e8d57ad56f2f4
Mastria <marco@mastria.com.br>
2018-10-09 00:18:56 -0300

    ror

--- system-obs/ror.py
+++ system-obs/ror.py
@@ -53,94 +53,78 @@ class RoR:
     def terminate(self):
         self._running = False
 
-    @property
     def _is_parked(self):
         if GPIO.input(ROR_PARKED) == GPIO.LOW:
             return True
         return False
 
-    @property
     def _is_unparked(self):
         if GPIO.input(ROR_PARKED) == GPIO.HIGH:
             return True
         return False
 
-    @property
     def is_not_closed(self):
         if GPIO.input(ROR_SW_CLOSED):
             return True
         return False
 
-    @property
     def _is_closed(self):
         if not GPIO.input(ROR_SW_CLOSED):
             return True
         return False
 
-    @property
     def _is_not_open(self):
         if GPIO.input(ROR_SW_OPEN):
             return True
         return False
 
-    @property
     def _is_open(self):
         if not GPIO.input(ROR_SW_OPEN):
             return True
         return False
 
-    @property
     def _is_mount_unparked(self):
         if GPIO.input(ROR_MOUNT_PARKED):
             return True
         return False
 
-    @property
     def _is_mount_parked(self):
         if not GPIO.input(ROR_MOUNT_PARKED):
             return True
         return False
 
-    @property
     def _is_stopped(self):
         if GPIO.input(ROR_MOVE) == GPIO.LOW:
             return True
         return False
 
-    @property
     def _is_moving(self):
         if GPIO.input(ROR_MOVE) == GPIO.HIGH:
             return True
         return False
 
-    @property
     def can_open(self):
         return self._is_unparked and self._is_not_open and self._is_mount_parked
 
-    @property
     def can_close(self):
         return self._is_unparked and self.is_not_closed and self._is_mount_parked
 
-    @property
     def can_park(self):
         return self._is_unparked and self._is_closed and self._is_not_open
 
-    @property
     def _can_unpark(self):
         return self._is_parked and self._is_mount_parked
 
-    @property
     def _can_move(self):
         return self._is_unparked and self._is_mount_parked
 
-    @property
     def _can_stop(self):
         return self.is_not_closed and self._is_not_open
 
     def _move(self):
         self.log.debug('move')
-        if self._can_move:
-            if self._is_moving:
+        if self._can_move():
+            if self._is_moving():
                 GPIO.output(ROR_MOVE, GPIO.LOW)
                 sleep(1)
             GPIO.output(ROR_MOVE, GPIO.HIGH)
@@ -149,10 +133,10 @@ class RoR:
 
     def _stop(self):
         self.log.debug('stop')
-        if self._is_moving:
+        if self._is_moving():
             GPIO.output(ROR_MOVE, GPIO.LOW)
             sleep(1)
-        if self._can_stop:
+        if self._can_stop():
             GPIO.output(ROR_MOVE, GPIO.HIGH)
             sleep(1)
             GPIO.output(ROR_MOVE, GPIO.LOW)
@@ -160,51 +144,51 @@ class RoR:
 
     def park(self):
         self.log.debug('park')
-        if self.is_not_closed:
+        if self.is_not_closed():
             if not self.close():
                 return False
-        if self.can_park:
+        if self.can_park():
             GPIO.output(ROR_PARKED, GPIO.LOW)
             return True
         return False
 
     def unpark(self):
         self.log.debug('unpark')
-        if self._can_unpark:
+        if self._can_unpark():
             GPIO.output(ROR_PARKED, GPIO.HIGH)
             return True
         return False
 
     def open(self):
         self.log.debug('open')
-        if self.can_open:
+        if self.can_open():
             self._move()
-            while self._is_closed:
+            while self._is_closed():
                 sleep(1)
-            while self._is_not_open and self._is_moving:
+            while self._is_not_open() and self._is_moving():
                 sleep(1)
-                if self._is_closed:
+                if self._is_closed():
                     self._move()
-                    while self._is_closed and self._is_moving:
+                    while self._is_closed() and self._is_moving():
                         sleep(1)
-            if self._is_moving:
+            if self._is_moving():
                 GPIO.output(ROR_MOVE, GPIO.LOW)
             return True
         return False
 
     def close(self):
         self.log.debug('close')
-        if self.can_close:
+        if self.can_close():
             self._move()
-            while self._is_open:
+            while self._is_open():
                 sleep(1)
-            while self.is_not_closed and self._is_moving:
+            while self.is_not_closed() and self._is_moving():
                 sleep(1)
-                if self._is_open:
+                if self._is_open():
                     self._move()
-                    while self._is_open and self._is_moving:
+                    while self._is_open() and self._is_moving():
                         sleep(1)
-            if self._is_moving:
+            if self._is_moving():
                 GPIO.output(ROR_MOVE, GPIO.LOW)
             return True
         return False
@@ -216,11 +200,11 @@ class RoR:
     # 0/1 for unparked/parked, 0/1 for closed/open shutter and azimuth as float.
     @property
     def status(self):
-        p = '1' if self._is_parked else '0'
-        s = '1' if self._is_open else '0' if self._is_closed else '-'
-        m = '1' if self._is_mount_parked else '0'
+        p = '1' if self._is_parked() else '0'
+        s = '1' if self._is_open() else '0' if self._is_closed() else '-'
+        m = '1' if self._is_mount_parked() else '0'
         self.log.debug('-- parked: %s (p:%s) - open: %s - closed: %s (s:%s) - mount_parked: %s (m:%s)' % (
-            self._is_parked, p, self._is_open, self._is_closed, s, self._is_mount_parked, m))
+            self._is_parked(), p, self._is_open(), self._is_closed(), s, self._is_mount_parked(), m))
         return '%s %s 0' % (p, s)
 
 # --------------------------
@@ -228,6 +212,7 @@ class RoR:
 # 200 OK
 # 202 ACCEPTED
 # 409 CONFLICT 
+
 
 @route('/', method='GET')
 def index():
@@ -238,7 +223,7 @@ def index():
 
 @route('/park', method='GET')
 def can_park():
-    response.status = 200 if RoR.is_not_closed or RoR.can_park else 409
+    response.status = 200 if RoR.is_not_closed() or RoR.can_park() else 409
     return
 
 
@@ -262,7 +247,7 @@ def unpark():
 
 @route('/open', method='GET')
 def can_open():
-    response.status = 200 if RoR.can_open else 409
+    response.status = 200 if RoR.can_open() else 409
     return
 
 
@@ -277,7 +262,7 @@ def open():
 
 @route('/close', method='GET')
 def can_close():
-    response.status = 200 if RoR.can_close else 409
+    response.status = 200 if RoR.can_close() else 409
     return
 
 
@@ -321,4 +306,3 @@ if __name__ == "__main__":
 
     log.debug("starting roll-off roof controller")
     run(host='0.0.0.0', port=80, server='gevent')
-
