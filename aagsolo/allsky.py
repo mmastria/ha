945cc67429e5fe764e21963324748722ac4b778f
root <root@aagsolo>
2018-10-10 06:25:44 +0200

    allsky

---
+++ aagsolo/allsky.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import pygame
+import pygame.camera
+from PIL import Image
+
+pygame.camera.init()
+#pygame.camera.list_camera() #Camera detected or not
+cam = pygame.camera.Camera("/dev/video0",(640,480))
+cam.start()
+img = cam.get_image()
+pygame.image.save(img,"/tmp/allskyfull.jpg")
+img = Image.open("/tmp/allskyfull.jpg")
+img = img.crop(( 86, 4, 550, 400))
+img.save("/home/aagsolo/www/allsky.jpg")
+
