f817adfae70b81263bb0d643a68424963a510b86
root <marco@mastria.com.br>
2018-10-13 16:56:38 -0300

    control box

--- system-obs/setup2.sh
+++ system-obs/setup2.sh
@@ -5,12 +5,12 @@
 useradd -m -s /bin/bash obs && \
 adduser obs dialout && \
 adduser obs gpio && \
+mkdir /usr/share/indi/scripts && \
 pip install bottle && \
 pip install -U 'gevent~=1.2.2' && \
 pip install multiprocessing && \
 sed -i 's/^#alias l/alias l/g' /home/obs/.bashrc
 
-mkdir /usr/share/indi/scripts
 cp -f *.service /etc/systemd/system/
 cp -f arua*.sh /usr/local/bin/
 cp -f *.py /usr/share/indi/scripts/
