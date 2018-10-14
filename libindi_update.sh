bbd54d6cc64f06d4369427f7ac5b8d5b3f506856
root <marco@mastria.com.br>
2018-10-14 19:58:02 -0300

    control box

--- libindi_update.sh
+++ libindi_update.sh
@@ -2,7 +2,7 @@
 pushd ~
 [ -d libindi.old ] && rm -rf libindi.old
 [ -f libindi_rpi.tar.gz ] && mkdir libindi.old && mv libindi_* libindi.old/
-curl http://indilib.org/download/raspberry-pi/send/6-raspberry-pi/9-indi-library-for-raspberry-pi.html -o libindi_rpi.tar.gz
+curl -k https://indilib.org/download/raspberry-pi/send/6-raspberry-pi/9-indi-library-for-raspberry-pi.html -o libindi_rpi.tar.gz
 tar -zxvf libindi_rpi.tar.gz
 echo ""
 echo "*** VERIFICANDO ATUALIZACOES ***"
