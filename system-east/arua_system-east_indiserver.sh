d5f162eb02f9b2ab1b49c2780adfa13a3b184e6c
root <marco@mastria.com.br>
2018-10-15 23:12:49 -0300

    add system-obs

--- system-east/arua_system-east_indiserver.sh
+++ system-east/arua_system-east_indiserver.sh
@@ -19,12 +19,12 @@ echo "start indi_ieq_telescope -n \"iOptron CEM60\" -c \"$HOME/.indi/iOptron CEM
 echo "start indi_atik_ccd -n \"Atik 383L+\" -c \"$HOME/.indi/Atik 383L+_config.xml\"" > ~/.indi/indiFIFO
 echo "start indi_sx_wheel -n \"SX Wheel\" -c \"$HOME/.indi/SX Wheel_config.xml\"" > ~/.indi/indiFIFO
 echo "start indi_qhy_ccd -n \"QHY5LII-M\" -c \"$HOME/.indi/QHY5LII-M_config.xml\"" > ~/.indi/indiFIFO
-echo "start \"Armadillo focuser\"@system-main:7625" > ~/.indi/indiFIFO
-echo "start \"Astrometry\"@system-main:7625" > ~/.indi/indiFIFO
-echo "start \"AAG Cloud Watcher\"@aagsolo" > ~/.indi/indiFIFO
-echo "start \"RollOff Roof\"@system-power:7625" > ~/.indi/indiFIFO
-echo "start \"Vantage\"@aagsolo:7625" > ~/.indi/indiFIFO
-echo "start \"V4L2 CCD\"@aagsolo:7626" > ~/.indi/indiFIFO
+echo "start \"Armadillo focuser Main\"@system-obs:7624" > ~/.indi/indiFIFO
+echo "start \"Astrometry\"@system-obs:7624" > ~/.indi/indiFIFO
+#echo "start \"AAG Cloud Watcher\"@system-weather:7624" > ~/.indi/indiFIFO
+#echo "start \"Vantage\"@system-weather:7624" > ~/.indi/indiFIFO
+echo "start \"V4L2 CCD\"@system-obs:7624" > ~/.indi/indiFIFO
+echo "start \"Dome Scripting Gateway\"@system-obs:7624" > ~/.indi/indiFIFO
 
 wait "$child"
 echo "stopping indiserver"
