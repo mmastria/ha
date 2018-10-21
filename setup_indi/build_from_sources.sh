83436288deae3bdebb34c182f0c3d538864bb819
mmastria <marco@mastria.com.br>
2018-10-21 03:20:51 -0300

    refactoring

--- setup_indi/build_from_sources.sh
+++ setup_indi/build_from_sources.sh
@@ -17,10 +17,10 @@ make install
 
 # 3rd party
 cd ~/indi/3rdparty/
-cmake-DCMAKE_CXX_FLAGS="-Wno-psabi" -DCMAKE_INSTALL_PREFIX=/usr .
+cmake -DCMAKE_CXX_FLAGS="-Wno-psabi" -DCMAKE_INSTALL_PREFIX=/usr .
 make
 make install
-cmake-DCMAKE_CXX_FLAGS="-Wno-psabi" -DCMAKE_INSTALL_PREFIX=/usr .
+cmake -DCMAKE_CXX_FLAGS="-Wno-psabi" -DCMAKE_INSTALL_PREFIX=/usr .
 make
 make install
 
@@ -35,11 +35,12 @@ ldconfig
 # Indi WiringPi GPIO
 cd
 git clone https://github.com/magnue/indi_wiringpi_gpio.git
-cp -f ~/ha/setup_indi/wiringpi_gpio.cpp ~/indi_wiringpi_gpio/wiringpi_gpio/
+diff -q ~/ha/setup_indi/wiringpi_gpio.cpp.ORIGINAL ~/indi_wiringpi_gpio/wiringpi_gpio/wiringpi_gpio.cpp && \
+cp -f ~/ha/setup_indi/wiringpi_gpio.cpp.NEW ~/indi_wiringpi_gpio/wiringpi_gpio/wiringpi_gpio.cpp
 cd indi_wiringpi_gpio
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=/usr . ..
+cmake -DCMAKE_CXX_FLAGS="-Wno-psabi" -DCMAKE_INSTALL_PREFIX=/usr . ..
 make install
 
 # CloudMakers
@@ -52,13 +53,10 @@ wget http://download.cloudmakers.eu/usbfocus-0.9-armhf.deb
 dpkg -i *.deb
 
 # PyIndi
-pip2  install --user --install-option="--prefix=/usr/local" pyindi-client
+pip install --install-option="--prefix=/usr/local" pyindi-client
 
 # apt -y install indi-full
 # pip2 install --upgrade pip
-
-# . ./libindi_update.sh
-# pip install --install-option="--prefix=/usr/local" pyindi-client
 # apt-get -y install python-rpi.gpio python-requests
 
 
