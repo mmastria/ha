2c3f81dcc8805300fb12ec085fb2fd4496b4321e
mmastria <marco@mastria.com.br>
2018-10-27 02:32:23 -0300

    IP Camera

--- setup_indi/build_from_sources.sh
+++ setup_indi/build_from_sources.sh
@@ -16,6 +16,8 @@ cp -f ~/ha/setup_indi/dome_script.cpp.NEW ~/indi/libindi/drivers/dome/dome_scrip
 ~/indi/libindi/drivers/weather/weatherwatcher.cpp
 diff -q ~/ha/setup_indi/weatherwatcher.cpp.ORIGINAL ~/indi/libindi/drivers/weather/weatherwatcher.cpp  && \
 cp -f ~/ha/setup_indi/weatherwatcher.cpp.NEW ~/indi/libindi/drivers/weather/weatherwatcher.cpp
+diff -q ~/ha/setup_indi/vantage.cpp.ORIGINAL ~/indi/libindi/drivers/weather/vantage.cpp  && \
+cp -f ~/ha/setup_indi/vantage.cpp.NEW ~/indi/libindi/drivers/weather/vantage.cpp
 mkdir -p ~/indi/buid/libindi
 cd ~/indi/buid/libindi/
 #cmake -DCMAKE_CXX_FLAGS="-Wno-psabi -Wno-unused-parameter -Wno-unused-variable -Wno-unused-but-set-parameter -Wno-sign-compare -Wno-reorder -Wno-unused-value -Wno-sign-compare -Wno-misleading-indentation -Wno-maybe-uninitialized -Wno-unused-function" -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Debug ~/indi/libindi
