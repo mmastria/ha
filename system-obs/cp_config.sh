e0f2e9652586e5cc17c0acef0ce8031367f6d64e
mmastria <marco@mastria.com.br>
2018-10-29 01:56:56 -0300

    defaults

--- system-obs/cp_config.sh
+++ system-obs/cp_config.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ls -1 /home/obs/.indi/*_config.xml|xargs -r -n 1 -I{} cp {} {}.default
-ls -1 /home/obs/.indi/*_config.xml.default|xargs -r -n 1 -I{} cp {} .
+ls -1 /home/obs/.indi/*_config.xml|xargs -r -n 1 -I{} cp -v {} {}.default
+ls -1 /home/obs/.indi/*_config.xml.default|xargs -r -n 1 -I{} cp -v {} .
