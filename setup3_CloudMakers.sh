aec7ab859f9d4e0742f22912f08f92de98fe876d
root <marco@mastria.com.br>
2018-11-04 14:48:19 -0200

    scripts errors

--- setup3_CloudMakers.sh
+++ setup3_CloudMakers.sh
@@ -10,18 +10,9 @@ wget http://download.cloudmakers.eu/atikccd-1.30-armhf.deb
 #wget http://download.cloudmakers.eu/shoestring-1.3-armhf.deb
 #wget http://download.cloudmakers.eu/usbfocus-0.9-armhf.deb
 dpkg -i *.deb
-popd
-
-/usr/local to /usr for bin/indi_atik_ccd, bin/indi_atik_wheel and share/indi_atik.xml.
-root@system-east:~/ha# whereis indi_atik_ccd
-indi_atik_ccd: /usr/local/bin/indi_atik_ccd
-root@system-east:~/ha# whereis indi_v4l2_ccd
-indi_v4l2_ccd: /usr/bin/indi_v4l2_ccd
-root@system-east:~/ha# whereis indi_atik_wheel
-indi_atik_wheel: /usr/local/bin/indi_atik_wheel
-
 ln -s /usr/local/bin/indi_atik_ccd /usr/bin/indi_atik_ccd
 ln -s /usr/local/bin/atik_ccd_test /usr/bin/atik_ccd_test
 ln -s /usr/local/bin/indi_atik_wheel /usr/bin/indi_atik_wheel
 ln -s /usr/local/share/indi/indi_atik.xml /usr/share/indi/indi_atik.xml
 
+popd
--- setup3_CloudMakers_uninstall.sh
+++ setup3_CloudMakers_uninstall.sh
@@ -4,9 +4,9 @@ pushd ~
 cd CloudMakers
 ls -1 /etc/systemd/system/multi-user.target.wants/|grep arua|cut -d / -f 6|xargs -n 1 -r -t systemctl stop
 ls -1 /etc/systemd/system/multi-user.target.wants/|grep arua|cut -d / -f 6|xargs -n 1 -r -t systemctl disable
-rm ln /usr/bin/indi_atik_ccd
-rm ln /usr/bin/atik_ccd_test
-rm ln /usr/bin/indi_atik_wheel
+rm /usr/bin/indi_atik_ccd
+rm /usr/bin/atik_ccd_test
+rm /usr/bin/indi_atik_wheel
 rm /usr/share/indi/indi_atik.xml
 ls -1 *.deb|xargs -n 1 -r dpkg-deb --show|awk '{print $1}'|xargs -r dpkg -P
 #apt -y --fix-broken install
