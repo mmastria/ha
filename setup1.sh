e041e5e31629f3db2690b3b140f1dc0a20740445
root <marco@mastria.com.br>
2018-11-12 19:01:08 -0200

    refactor scripts

--- setup1.sh
+++ setup1.sh
@@ -89,7 +89,8 @@ apt-get -y install build-essential git python-dev python-pip vim cmake ntpdate \
        libftdi1-dev fxload libkrb5-dev libcurl4-gnutls-dev \
        libraw-dev libgphoto2-dev libgsl-dev dkms \
        libboost-regex-dev libgps-dev libdc1394-22-dev \
-       zlib1g-dev libffi-dev libfftw3-dev librtlsdr-dev ffmpeg gawk lsof libavcodec-dev libavdevice-dev
+       zlib1g-dev libffi-dev libfftw3-dev librtlsdr-dev ffmpeg gawk lsof libavcodec-dev libavdevice-dev \
+       astrometry.net astrometry-data*
 [ '$DEVICE' == 'aagsolo' ] && apt-get -y install swig 
 [ '$DEVICE' != 'aagsolo' ] && apt-get -y install swig2.0 libz3-dev
 apt-get -y --fix-broken install
