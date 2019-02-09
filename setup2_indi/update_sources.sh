2c139c0e04bd0bb540fd42a1db4dde73fb08438b
mmastria <marco@mastria.com.br>
2019-02-09 00:17:33 -0200

    update

--- setup2_indi/update_sources.sh
+++ setup2_indi/update_sources.sh
@@ -17,10 +17,10 @@ git pull
 
 # enquanto der erro na rtlsdr - updated libdspau 1.05, 2.0.5 - 09-jan-2019
 # revertendo para antes da atualizacao da libdspau - 03-nov-2018
-git checkout 8d839b89929926ae253772349246c727e2cc3987 ./3rdparty/indi-rtlsdr/indi_rtlsdr_detector.cpp
-git checkout 8d839b89929926ae253772349246c727e2cc3987 ./3rdparty/indi-rtlsdr/indi_rtlsdr_detector.cpp
+##git checkout 8d839b89929926ae253772349246c727e2cc3987 ./3rdparty/indi-rtlsdr/indi_rtlsdr_detector.cpp
+##git checkout 8d839b89929926ae253772349246c727e2cc3987 ./3rdparty/indi-rtlsdr/indi_rtlsdr_detector.cpp
 # erro em indi-radiosim
-git checkout f8b50d4257361517cb6234f748dc8939151f0a1f ./3rdparty/indi-radiosim/indi_radiosim_detector.cpp
+##git checkout f8b50d4257361517cb6234f748dc8939151f0a1f ./3rdparty/indi-radiosim/indi_radiosim_detector.cpp
 
 # WiringPi
 cd ~/wiringPi
