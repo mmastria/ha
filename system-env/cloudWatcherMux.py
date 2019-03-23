37ee77773367e6ee07947dd73e5ec57bc512823c
mmastria <marco@mastria.com.br>
2019-03-23 20:35:07 -0300

    ajdusting

--- system-env/cloudWatcherMux.py
+++ system-env/cloudWatcherMux.py
@@ -7,7 +7,7 @@ import subprocess
 
 def main():
 
-    print "\nInitializing serial relay connection"
+    print '\nInitializing serial relay connection'
 
     watcher = serial.Serial(
         port = '/dev/ttyUSB0',
@@ -70,53 +70,90 @@ def main():
     indi.flushInput()
     indi.flushOutput()
 
-    solo_req = "" 
-    indi_req = ""
+    solo_req = '' 
+    indi_req = ''
+
+    timeout = 100
+    handshaking = '\x21\x11\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x30'
 
     while True:
 
         inByte = ''
         while solo.inWaiting() > 0:
             inByte = solo.read(1)
+            if inByte == '':
+                break;
             solo_req += inByte
             if inByte == '!':
                 break;
         if inByte == '!':
-            print "solo >> " + solo_req
+            print 'solo >> ' + solo_req
             watcher.write(solo_req)
-            solo_req = "" 
             time.sleep(0.1)
-            watcher_resp = ""
-            while watcher.inWaiting() == 0:
-                pass
+            watcher_resp = ''
+            timeout = 100
+            if solo_req == 'E!':
+                time.sleep(0.2)
+                timeout *= 3
+            solo_req = '' 
+            while watcher.inWaiting() == 0 and timeout > 0:
+                time.sleep(0.001)
+                timeout -= 1
             while watcher.inWaiting() > 0:
-                watcher_resp += watcher.read(1)
-            if watcher_resp != '':
-                print "solo << " + watcher_resp
+                inByte = watcher.read(1)
+                if not (watcher_resp == '' and inByte != '!'):
+                    watcher_resp += inByte
+            if len(watcher_resp) >= 15 and len(watcher_resp) % 15 == 13 and watcher_resp[-13:]+' 0'==handshaking:
+                watcher_resp += ' 0'
+            if len(watcher_resp) >= 15 and len(watcher_resp) % 15 == 14 and watcher_resp[-14:]+'0'==handshaking:
+                watcher_resp += '0'
+            if len(watcher_resp) >= 15 and len(watcher_resp) % 15 == 0 and watcher_resp[-15:]==handshaking:
+                print 'solo << ' + watcher_resp
                 solo.write(watcher_resp)
-                time.sleep(0.1)
+            else:
+                print 'solo <# ' + watcher_resp.replace(' ','.')
+                print 'solo <= ' + handshaking
+                solo.write(handshaking)
+            time.sleep(0.1)
 
         inByte = '' 
         while indi.inWaiting() > 0:
             inByte = indi.read(1)
+            if inByte == '':
+                break;
             indi_req += inByte
             if inByte == '!':
                 break;
         if inByte == '!':
-            print "indi >> " + indi_req
+            print 'indi >> ' + indi_req
             watcher.write(indi_req)
-            indi_req = ""
             time.sleep(0.1)
-            watcher_resp = ""
-            while watcher.inWaiting() == 0:
-                pass
+            watcher_resp = ''
+            timeout = 100
+            if indi_req == 'E!':
+                time.sleep(0.2)
+                timeout *= 3
+            indi_req = ''
+            while watcher.inWaiting() == 0 and timeout > 0:
+                time.sleep(0.001)
+                timeout -= 1
             while watcher.inWaiting() > 0:
-                watcher_resp += watcher.read(1)
-            if watcher_resp != '':
-                print "indi << " + watcher_resp
+                inByte = watcher.read(1)
+                if not (watcher_resp == '' and inByte != '!'):
+                    watcher_resp += inByte
+            if len(watcher_resp) >= 15 and len(watcher_resp) % 15 == 13 and watcher_resp[-13:]+' 0'==handshaking:
+                watcher_resp += ' 0'
+            if len(watcher_resp) >= 15 and len(watcher_resp) % 15 == 14 and watcher_resp[-14:]+'0'==handshaking:
+                watcher_resp += '0'
+            if len(watcher_resp) >= 15 and len(watcher_resp) % 15 == 0 and watcher_resp[-15:]==handshaking: 
+                print 'indi << ' + watcher_resp
                 indi.write(watcher_resp)
-                time.sleep(0.1)
+            else:
+                print 'indi <# ' + watcher_resp.replace(' ','.')
+                print 'indi <= ' + handshaking
+                indi.write(handshaking)
+            time.sleep(0.1)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
     proc.kill() 
