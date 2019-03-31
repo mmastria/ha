26ebc51bfe60e97d0a6058b0513963034e2bae8a
mmastria <marco@mastria.com.br>
2019-03-31 19:15:24 -0300

    run

--- system-env/cloudWatcherMux.py
+++ system-env/cloudWatcherMux.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
 import serial
 import sys
 import time
 import os
 import subprocess
 
-timeout = 100
+timeout     = 100
 handshaking = '\x21\x11\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x30'
-kresponse =   '\x21\x4b\x21\x21\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
-mresponse =   '\x21\x4d\x21\x21\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
-hschar =      '\x11'
-RESP_V =      '!w          000'
-RESP_K    =   '!K1198\x20\x20\x20\x20\x20\x20\x20\x20\x20'
-RESP_M    =   '!M\x01\x22\x07\xD0\x02\x30\x0D\x7A\x00\x0A\x00\x0A\x00'
+kresponse   = '\x21\x4b\x21\x21\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+mresponse   = '\x21\x4d\x21\x21\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+hschar      = '\x11'
+RESP_V      = '!w          000'
+RESP_K      = '!K1198\x20\x20\x20\x20\x20\x20\x20\x20\x20'
+RESP_M      = '!M\x01\x22\x07\xD0\x02\x30\x0D\x7A\x00\x0A\x00\x0A\x00'
 
+# RESP_K
 # Internal Serial No         = 1198
-# Zener voltage              = 2.9  =  290 = x01 x22
-# LDR Max Resistance (K)     = 2000 = 2000 = x07 xD0
-# LDR PullUp Resistance (K)  = 56   =  560 = x02 x30
-# Rain Beta Factor           = 3450 = 3450 = x0D x7A
-# Rain Resistance at 25 (K)  = 1    =   10 = x00 x0A 
-# Rain PullUp Resistance (K) = 1    =   10 = x00 x0A
+
+# RESP_M
+# Zener voltage              =  2.9 * 10 =  290 = x01 x22
+# LDR Max Resistance (K)     = 2000 *  1 = 2000 = x07 xD0
+# LDR PullUp Resistance (K)  =   56 * 10 =  560 = x02 x30
+# Rain Beta Factor           = 3450 *  1 = 3450 = x0D x7A
+# Rain Resistance at 25 (K)  =    1 * 10 =   10 = x00 x0A 
+# Rain PullUp Resistance (K) =    1 * 10 =   10 = x00 x0A
 
 # Firmware ver. 5.70
 
@@ -35,14 +40,10 @@ def talk(src, sname, dest, dname):
         if inByte == '!':
             break;
     if inByte == '!':
-        print '\n' + sname + ' >> [' + src_req + ']'
-        print ' '*len(sname)+'    [' + ':'.join(x.encode('hex') for x in src_req) + ']'
+        #print '\n' + sname + ' >> [' + src_req + ']'
+        #print ' '*len(sname)+'    [' + ':'.join(x.encode('hex') for x in src_req) + ']'
         if src_req == 'V!':
             dest_resp = RESP_V + handshaking
-        #elif src_req == 'K!':
-        #    dest_resp = RESP_K + handshaking 
-        #elif src_req == 'M!':
-        #    dest_resp = RESP_M + handshaking 
         else:
             dest.write(src_req)
             time.sleep(0.1)
@@ -59,27 +60,27 @@ def talk(src, sname, dest, dname):
                 time.sleep(0.01)
                 if not (dest_resp == '' and inByte != '!'):
                     dest_resp += inByte
-                else:
-                    print ' '*len(sname) + ' ## [' + inByte + '] discarding - hex [' + inByte.encode('hex') + ']'
+                #else:
+                    #print ' '*len(sname) + ' ## [' + inByte + '] discarding - hex [' + inByte.encode('hex') + ']'
         if len(dest_resp) >= 15 and len(dest_resp) % 15 == 13 and dest_resp[-13:]+' 0' == handshaking:
             dest_resp += ' 0'
         if len(dest_resp) >= 15 and len(dest_resp) % 15 == 14 and dest_resp[-14:]+'0' == handshaking:
             dest_resp += '0'
         if len(dest_resp) >= 15 and len(dest_resp) % 15 == 0 and dest_resp[-15:] == handshaking:
             if src_req == 'K!' and dest_resp == kresponse + handshaking:
-                print sname + ' <# [' + dest_resp.replace(hschar,'_').replace(' ','.') + ']'
+                #print sname + ' <# [' + dest_resp.replace(hschar,'_').replace(' ','.') + ']'
                 dest_resp = RESP_K + handshaking 
             if src_req == 'M!' and dest_resp == mresponse + handshaking:
-                print sname + ' <# [' + dest_resp.replace(hschar,'_').replace(' ','.') + ']'
+                #print sname + ' <# [' + dest_resp.replace(hschar,'_').replace(' ','.') + ']'
                 dest_resp = RESP_M + handshaking 
-            print sname + ' << [' + dest_resp.replace(hschar,'_') + ']'
-            print ' '*len(sname)+'    [' + ':'.join(x.encode('hex') for x in dest_resp) + ']'
+            #print sname + ' << [' + dest_resp.replace(hschar,'_') + ']'
+            #print ' '*len(sname)+'    [' + ':'.join(x.encode('hex') for x in dest_resp) + ']'
             src.write(dest_resp)
         else:
-            print sname + ' <# [' + dest_resp.replace(hschar,'_').replace(' ','.') + ']'
-            print ' '*len(sname)+'    [' + ':'.join(x.encode('hex') for x in dest_resp) + ']'
-            print sname + ' <= [' + handshaking.replace(hschar,'_') + ']'
-            print ' '*len(sname)+'    [' + ':'.join(x.encode('hex') for x in handshaking) + ']'
+            #print sname + ' <# [' + dest_resp.replace(hschar,'_').replace(' ','.') + ']'
+            #print ' '*len(sname)+'    [' + ':'.join(x.encode('hex') for x in dest_resp) + ']'
+            #print sname + ' <= [' + handshaking.replace(hschar,'_') + ']'
+            #print ' '*len(sname)+'    [' + ':'.join(x.encode('hex') for x in handshaking) + ']'
             src.write(handshaking)
         time.sleep(0.1)
 
@@ -123,7 +124,6 @@ def main():
     solo = connect( '/dev/ttyAMA0', 'AAG Solo')
     indi = connect( '/dev/ttyUSB1', 'Indi' )
 
-
     print 'Serial Multiplex running...'
     while True:
 
@@ -134,5 +134,13 @@ def main():
         talk( indi, 'indi', watcher, 'watcher')
 
 if __name__ == '__main__':
-    main()
-    destroyVPort(proc)
+    try:
+        main()
+    except:
+        pass
+    try:
+        destroyVPort(proc)
+    except:
+        pass
+    print '\nSerial Multiplex ended!\n'
+
