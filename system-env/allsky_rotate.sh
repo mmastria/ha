bbaffc501743cec32419d90ef4b54be675e642ea
mmastria <marco@mastria.com.br>
2019-04-12 10:42:11 -0300

    allsky rotate

--- system-env/allsky_rotate.sh
+++ system-env/allsky_rotate.sh
@@ -3,6 +3,6 @@ cd /var/www/html
 FILE="allsky_$(date -d 'yesterday 13:00' +%Y%m%d)"
 FILELIST="${FILE}-*.jpg"
 ffmpeg -y -pattern_type glob -i ${FILE}'-*.jpg' -vf "pad=ceil(iw/2)*2:ceil(ih/2)*2" -pix_fmt yuv420p ${FILE}.mp4
-tar -rf ${FILE}.tgz $FILELIST
+tar -zcvf ${FILE}.tgz $FILELIST
 rm -f $FILELIST
 
