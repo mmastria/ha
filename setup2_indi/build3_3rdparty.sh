681a72364850c2747f681e6f73065d332cc640de
mmastria <marco@mastria.com.br>
2019-04-20 15:45:48 -0300

    obs

--- setup2_indi/build3_3rdparty.sh
+++ setup2_indi/build3_3rdparty.sh
@@ -4,14 +4,14 @@ pushd ~
 
 [ -d ~/indi ] || exit 1 
 
-[ -f ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.cpp.ORIGINAL ] || cp ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.cpp ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.cpp.ORIGINAL
-[ -f ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.h.ORIGINAL ] || cp ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.h ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.h.ORIGINAL
+#[ -f ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.cpp.ORIGINAL ] || cp ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.cpp ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.cpp.ORIGINAL
+#[ -f ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.h.ORIGINAL ] || cp ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.h ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.h.ORIGINAL
 
-diff -q ~/ha/setup2_indi/arm_plat_focuser_common.cpp.ORIGINAL ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.cpp.ORIGINAL || exit 1
-diff -q ~/ha/setup2_indi/arm_plat_focuser_common.h.ORIGINAL ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.h.ORIGINAL || exit 1
+#diff -q ~/ha/setup2_indi/arm_plat_focuser_common.cpp.ORIGINAL ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.cpp.ORIGINAL || exit 1
+#diff -q ~/ha/setup2_indi/arm_plat_focuser_common.h.ORIGINAL ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.h.ORIGINAL || exit 1
 
-cp -f ~/ha/setup2_indi/arm_plat_focuser_common.cpp.NEW ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.cpp
-cp -f ~/ha/setup2_indi/arm_plat_focuser_common.h.NEW ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.h
+#cp -f ~/ha/setup2_indi/arm_plat_focuser_common.cpp.NEW ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.cpp
+#cp -f ~/ha/setup2_indi/arm_plat_focuser_common.h.NEW ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.h
 
 # 3rd party
 cd ~/indi/3rdparty/
--- setup2_indi/update_sources.sh
+++ setup2_indi/update_sources.sh
@@ -8,18 +8,65 @@ pushd ~
 
 [ -f ~/indi/libindi/libs/indibase/indidome.cpp.ORIGINAL ] && mv -f ~/indi/libindi/libs/indibase/indidome.cpp.ORIGINAL ~/indi/libindi/libs/indibase/indidome.cpp
 [ -f ~/indi/libindi/drivers/dome/dome_script.cpp.ORIGINAL ] && mv -f ~/indi/libindi/drivers/dome/dome_script.cpp.ORIGINAL ~/indi/libindi/drivers/dome/dome_script.cpp
-[ -f ~/indi/libindi/drivers/weather/weatherwatcher.cpp.ORIGINAL ] && mv -f ~/indi/libindi/drivers/weather/weatherwatcher.cpp.ORIGINAL ~/indi/libindi/drivers/weather/weatherwatcher.cpp
 
-[ -f ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.cpp.ORIGINAL ] && mv -f ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.cpp.ORIGINAL ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.cpp
-[ -f ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.h.ORIGINAL ] && mv -f ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.h.ORIGINAL ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.h
+#[ -f ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.cpp.ORIGINAL ] && mv -f ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.cpp.ORIGINAL ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.cpp
+#[ -f ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.h.ORIGINAL ] && mv -f ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.h.ORIGINAL ~/indi/3rdparty/indi-armadillo-platypus/arm_plat_focuser_common.h
 
 [ -f ~/indi_wiringpi_gpio/wiringpi_gpio/wiringpi_gpio.cpp.ORIGINAL ] && mv -f ~/indi_wiringpi_gpio/wiringpi_gpio/wiringpi_gpio.cpp.ORIGINAL ~/indi_wiringpi_gpio/wiringpi_gpio/wiringpi_gpio.cpp
 
 # libindi
 # 3rd party
 cd ~/indi
-git stash 
+
+# --------
+rm -f libindi/drivers/weather/weatherwatcher.cpp
+git checkout libindi/drivers/weather/weatherwatcher.cpp
+#git stash 
+# --------
 git pull
+# --------
+## TEMP
+#sed -i '0,/keywordT\[1\]/{s/minOK = -10/minOK = -10/}' libindi/drivers/weather/weatherwatcher.cpp
+#sed -i '0,/keywordT\[1\]/{s/maxOK = 30/maxOK = 1/}' libindi/drivers/weather/weatherwatcher.cpp
+#sed -i '0,/keywordT\[1\]/{s/percWarn = 15/percWarn = 0/}' libindi/drivers/weather/weatherwatcher.cpp
+## WIND
+#sed -i '0,/keywordT\[2\]/{s/minOK = 0/minOK = -1/}' libindi/drivers/weather/weatherwatcher.cpp
+#sed -i '0,/keywordT\[2\]/{s/maxOK = 20/maxOK = -1/}' libindi/drivers/weather/weatherwatcher.cpp
+#sed -i '0,/keywordT\[2\]/{s/percWarn = 15/percWarn = 0/}' libindi/drivers/weather/weatherwatcher.cpp
+## GUST
+#sed -i '0,/keywordT\[3\]/{s/minOK = 0/minOK = -1/}' libindi/drivers/weather/weatherwatcher.cpp
+#sed -i '0,/keywordT\[3\]/{s/maxOK = 20/maxOK = -1/}' libindi/drivers/weather/weatherwatcher.cpp
+#sed -i '0,/keywordT\[3\]/{s/percWarn = 15/percWarn = 0/}' libindi/drivers/weather/weatherwatcher.cpp
+## RAIN 
+#sed -i '0,/keywordT\[0\]/{s/minOK = 0/minOK = 2550/}' libindi/drivers/weather/weatherwatcher.cpp
+#sed -i '0,/keywordT\[0\]/{s/maxOK = 0/maxOK = 3000/}' libindi/drivers/weather/weatherwatcher.cpp
+#sed -i '0,/keywordT\[0\]/{s/percWarn = 15/percWarn = 5/}' libindi/drivers/weather/weatherwatcher.cpp
+## FORECAST 
+#sed -i '0,/keywordT\[4\]/{s/minOK = 0/minOK = 0/}' libindi/drivers/weather/weatherwatcher.cpp
+#sed -i '0,/keywordT\[4\]/{s/maxOK = 0/maxOK = 1/}' libindi/drivers/weather/weatherwatcher.cpp
+#sed -i '0,/keywordT\[4\]/{s/percWarn = 15/percWarn = 0/}' libindi/drivers/weather/weatherwatcher.cpp
+
+## TEMP
+#sed -i '/keywordT\[1\]/,/minOK/s/-10/-10/' libindi/drivers/weather/weatherwatcher.cpp
+#sed -i '/keywordT\[1\]/,/maxOK/s/30/1/' libindi/drivers/weather/weatherwatcher.cpp
+#sed -i '/keywordT\[1\]/,/percWarn/s/15/0/' libindi/drivers/weather/weatherwatcher.cpp
+## WIND
+#sed -i '/keywordT\[2\]/,/minOK/s/0/-1/' libindi/drivers/weather/weatherwatcher.cpp
+#sed -i '/keywordT\[2\]/,/maxOK/s/20/-1/' libindi/drivers/weather/weatherwatcher.cpp
+#sed -i '/keywordT\[2\]/,/percWarn/s/15/0/' libindi/drivers/weather/weatherwatcher.cpp
+## GUST
+#sed -i '/keywordT\[3\]/,/minOK/s/0/-1/' libindi/drivers/weather/weatherwatcher.cpp
+#sed -i '/keywordT\[3\]/,/maxOK/s/20/-1/' libindi/drivers/weather/weatherwatcher.cpp
+#sed -i '/keywordT\[3\]/,/percWarn/s/15/0/' libindi/drivers/weather/weatherwatcher.cpp
+## RAIN 
+#sed -i '/keywordT\[0\]/,/minOK/s/0/2550/' libindi/drivers/weather/weatherwatcher.cpp
+#sed -i '/keywordT\[0\]/,/maxOK/s/0/3000/' libindi/drivers/weather/weatherwatcher.cpp
+#sed -i '/keywordT\[0\]/,/percWarn/s/15/5/' libindi/drivers/weather/weatherwatcher.cpp
+## FORECAST 
+#sed -i '/keywordT\[4\]/,/minOK/s/0/0/' libindi/drivers/weather/weatherwatcher.cpp
+#sed -i '/keywordT\[4\]/,/maxOK/s/0/1/' libindi/drivers/weather/weatherwatcher.cpp
+#sed -i '/keywordT\[4\]/,/percWarn/s/15/0/' libindi/drivers/weather/weatherwatcher.cpp
+# --------
 
 # enquanto der erro na rtlsdr - updated libdspau 1.05, 2.0.5 - 09-jan-2019
 # revertendo para antes da atualizacao da libdspau - 03-nov-2018
