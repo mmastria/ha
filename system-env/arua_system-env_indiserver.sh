940c5034b263bd847f792ebc5f79dd0155470778
mmastria <marco@mastria.com.br>
2019-04-22 01:18:56 -0300

    env

--- system-env/arua_system-env_indiserver.sh
+++ system-env/arua_system-env_indiserver.sh
@@ -22,12 +22,12 @@ echo "start indi_vantage_weather -n \"Vantage\" -c \"$HOME/.indi/Vantage_config.
 echo "start indi_weather_safety_proxy -n \"Weather Safety Proxy\" -c \"$HOME/.indi/Weather Safety Proxy_config.xml\"" > ~/.indi/indiFIFO
 echo "start indi_meta_weather -n \"Weather Meta\" -c \"$HOME/.indi/Weather Meta_config.xml\"" > ~/.indi/indiFIFO
 echo "start indi_watchdog -n \"WatchDog\" -c \"$HOME/.indi/WatchDog_config.xml\"" > ~/.indi/indiFIFO
-#echo "start indi_rtlsdr_detector -n \"RTL-SDR Receiver\" -c \"$HOME/.indi/RTL-SDR Receiver_config.xml\"" > ~/.indi/indiFIFO
+echo "start indi_rtlsdr_detector -n \"RTL-SDR Receiver\" -c \"$HOME/.indi/RTL-SDR Receiver_config.xml\"" > ~/.indi/indiFIFO
 # http://[ipofcamera]:8080/stream/video/mjpeg
 # wget --user=admin --password=YWRtaW4= http://[ipofcamera]:8080/stream/snapshot.jpg
 echo "start indi_webcam_ccd -n \"Observ Cam\" -c \"$HOME/.indi/Observ Cam_config.xml\"" > ~/.indi/indiFIFO
 
-#/usr/share/indi/scripts/indi_watcher_weather.py &
+/usr/share/indi/scripts/indi_watcher_weather.py &
 /usr/share/indi/scripts/indi_aagcloudwatcher.py &
 /usr/share/indi/scripts/indi_vantage_weather.py &
 /usr/share/indi/scripts/indi_meta_weather.py
