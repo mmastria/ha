12cee4d38a10c27538118313cf290d3a25459579
mmastria <marco@mastria.com.br>
2019-05-05 22:55:39 -0300

    node-red

--- node-red/setup2.sh
+++ node-red/setup2.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 bash <(curl -sL https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/update-nodejs-and-nodered)
+apt install -y mosquitto mosquitto-clients
+
