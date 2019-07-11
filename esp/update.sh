e30f5b9abca2cdf1f2947e7243c31cefa196adf9
marco.mastria@foursys.com.br <marco.mastria@foursys.com.br>
2019-07-11 00:30:11 -0300

    esp

---
+++ esp/update.sh
@@ -0,0 +1,8 @@
+# beta
+#pip2 install --pre -U esphome
+
+# dev
+# pip install https://github.com/esphome/esphome/archive/dev.zip
+
+sudo pip2 install -U esphome
+sudo pip2 install -U tornado esptool
