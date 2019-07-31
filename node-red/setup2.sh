9c58a152f8b3d0d4e90b09dfd19a2f65e52d5cab
mmastria <marco@mastria.com.br>
2019-07-30 22:15:25 -0300

    system-env update

--- node-red/setup2.sh
+++ node-red/setup2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 bash <(curl -sL https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/update-nodejs-and-nodered)
-apt install -y mosquitto mosquitto-clients libavahi-compat-libdnssd-dev
+#apt install -y mosquitto mosquitto-clients libavahi-compat-libdnssd-dev
 
--- system-env/arua_system-env_indiserver.sh
+++ system-env/arua_system-env_indiserver.sh
@@ -17,21 +17,20 @@ mkfifo ~/.indi/indiFIFO
 child=$!
 
 echo "start indi_watcher_weather -n \"AAG Solo Weather\" -c \"$HOME/.indi/AAG Solo Weather_config.xml\"" > ~/.indi/indiFIFO
-#echo "start indi_aagcloudwatcher -n \"AAG Cloud Watcher\" -c \"$HOME/.indi/AAG Cloud Watcher_config.xml\"" > ~/.indi/indiFIFO
 echo "start indi_vantage_weather -n \"Vantage\" -c \"$HOME/.indi/Vantage_config.xml\"" > ~/.indi/indiFIFO
 echo "start indi_weather_safety_proxy -n \"Weather Safety Proxy\" -c \"$HOME/.indi/Weather Safety Proxy_config.xml\"" > ~/.indi/indiFIFO
 echo "start indi_meta_weather -n \"Weather Meta\" -c \"$HOME/.indi/Weather Meta_config.xml\"" > ~/.indi/indiFIFO
 echo "start indi_watchdog -n \"WatchDog\" -c \"$HOME/.indi/WatchDog_config.xml\"" > ~/.indi/indiFIFO
-#echo "start indi_rtlsdr_detector -n \"RTL-SDR Receiver\" -c \"$HOME/.indi/RTL-SDR Receiver_config.xml\"" > ~/.indi/indiFIFO
 # http://[ipofcamera]:8080/stream/video/mjpeg
 # wget --user=admin --password=YWRtaW4= http://[ipofcamera]:8080/stream/snapshot.jpg
 echo "start indi_webcam_ccd -n \"Observ Cam\" -c \"$HOME/.indi/Observ Cam_config.xml\"" > ~/.indi/indiFIFO
+#echo "start indi_rtlsdr_detector -n \"RTL-SDR Receiver\" -c \"$HOME/.indi/RTL-SDR Receiver_config.xml\"" > ~/.indi/indiFIFO
 
 /usr/share/indi/scripts/loadDriver.py "AAG Solo Weather"
-#/usr/share/indi/scripts/loadDriver.py "AAG Cloud Watcher"
-/usr/share/indi/scripts/loadDriver.py "Weather Safety Proxy"
 /usr/share/indi/scripts/loadDriver.py "Vantage"
+/usr/share/indi/scripts/loadDriver.py "Weather Safety Proxy"
 /usr/share/indi/scripts/loadDriver.py "Weather Meta"
+#/usr/share/indi/scripts/loadDriver.py "RTL-SDR Receiver"
 
 wait "$child"
 echo "stopping indiserver"
