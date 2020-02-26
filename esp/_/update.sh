fea49955e5f9020ec3fcd2e575d725b87e37a947
marco.mastria@foursys.com.br <marco.mastria@foursys.com.br>
2020-02-26 20:47:51 -0300

    updated

---
+++ esp/_/update.sh
@@ -0,0 +1,19 @@
+# beta
+#pip install --pre -U esphome
+
+# dev
+# pip install https://github.com/esphome/esphome/archive/dev.zip
+
+#
+# versoes acima da 1.13.3 ate 1.13.6 com problemas de rede
+#
+#sudo pip2 install esphome==1.13.3
+#sudo pip2 install -U tornado esptool
+#sudo pip2 install voluptuous==0.11.5
+#platformio platform update
+
+pip install -U esphome
+pip install -U tornado esptools
+pip install -U voluptuous
+pip install -U platformio
+
